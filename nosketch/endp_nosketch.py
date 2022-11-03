3e2cfb2f68c4171db24f0d99795981140a306cd2
magistermilitum <37591896+magistermilitum@users.noreply.github.com>
2022-11-03 16:40:54 +0100

    Update endp_nosketch.py

--- nosketch/endp_nosketch.py
+++ nosketch/endp_nosketch.py
@@ -24,6 +24,16 @@ from matplotlib import pyplot as plt
 from bs4 import BeautifulSoup
 
 import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 
